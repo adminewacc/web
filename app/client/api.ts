12cc4950992c847b32017af9e2acf70a82b75424
admi <83762834+adminewacc@users.noreply.github.com>
2024-04-21 19:26:24 -0300

    Update api.ts

--- app/client/api.ts
+++ app/client/api.ts
@@ -123,7 +123,7 @@ export class ClientApi {
         {
           from: "human",
           value:
-            "Share from [NextChat]: https://github.com/adminewacc/opinform",
+            "Shared from OpinForm: https://github.com/adminewacc/opinform",
         },
       ]);
     // 敬告二开开发者们，为了开源大模型的发展，请不要修改上述消息，此消息用于后续数据清洗使用
